fix: corrected error in the calculation of M

kitaev/assemblers.py
@@ -17,122 +17,161 @@ def kit_hamiltonian(mu: float, t: float, delta: float, sites: int) -> np.ndarray
 
     Returns
     -------
-    A square matrix being the Kitaev Hamiltonian in the Majorana representation.
+    The antisymmetric square matrix H being the Kitaev Hamiltonian in the Majorana representation.
     """
-    h = np.zeros([2*sites, 2*sites], dtype=np.complex128)
+    H = np.zeros([2*sites, 2*sites], dtype=np.complex128)
     jx = t - delta
     jy = t + delta
 
     PBC = True
 
     for n in range(0, sites - 1):
-        h[2*n, 2*n + 1] = mu
-        h[2*n + 1, 2*n] = -mu
-        h[2*n + 3, 2*n] = jx
-        h[2*n, 2*n + 3] = -jx
-        h[2*n + 1, 2*n + 2] = -jy
-        h[2*n + 2, 2*n + 1] = jy
+        H[2*n, 2*n + 1] = mu
+        H[2*n + 1, 2*n] = -mu
+        H[2*n + 3, 2*n] = -jx
+        H[2*n, 2*n + 3] = jx
+        H[2*n + 1, 2*n + 2] = -jy
+        H[2*n + 2, 2*n + 1] = jy
 
-    h[2*(sites - 1), 2*(sites - 1) + 1] = mu
-    h[2*(sites - 1) + 1, 2*(sites - 1)] = -mu
+    H[2*(sites - 1), 2*(sites - 1) + 1] = mu
+    H[2*(sites - 1) + 1, 2*(sites - 1)] = -mu
 
     if PBC:
-        h[0, 2*sites - 1] = jy
-        h[2*sites - 1, 0] = -jy
-        h[2*sites - 2, 1] = jx
-        h[2, 2*sites - 2] = -jx
+        H[0, 2*sites - 1] = jy
+        H[2*sites - 1, 0] = -jy
+        H[2*sites - 2, 1] = jx
+        H[1, 2*sites - 2] = -jx
 
-    h = (1j/4)*h
+    H = (1j/4)*H
 
-    assert (h.transpose == -h).all, "Majorana Hamiltonian is not antisymmetric!"
+    assert (H.T == -H).all, "Majorana Hamiltonian is not antisymmetric!"
 
-    return h
+    visualization = False
+    if visualization:
+        fig, ax = plt.subplots()
+        ax.matshow(H.imag)
+        for (i, j), z in np.ndenumerate(H.imag):
+            ax.text(j, i, '{:0.3f}'.format(z), ha='center', va='center')
+        fig.suptitle('Kitaev Hamiltonian with $\mu=%.2f$, $t=%.2f$, $\Delta=%.2f$ and $%d$ sites' % (mu, t, delta, sites))
+
+    return H
 
 
-def bath_operators(gamma_g: float, gamma_l: float, sites: int) -> np.ndarray:
+def dissipator(gamma_g: float, gamma_l: float, sites: int) -> np.ndarray:
     """
-    Calculates the matrix l encoding dissipation in the Lindblad approximation, for baths that
-    create and annihilate fermions at the same rate (i.e. the jump operators are Hermitian).
+    Calculates the matrix M encoding dissipation in the Lindblad approximation, for baths that
+    create and annihilate fermions on each site.
 
     Parameters
     ----------
-    gamma: rate of creation/annihilation of fermions
+    gamma_g: rate of fermion creation
+    gamma_l: rate of fermion annihilation
     sites: number of sites of the Kitaev chain
 
     Returns
     -------
-    The bath operators in Majorana representation
+    The Hermitian square matrix M, describing the effect of dissipation as a result on the interaction with the
+    environment.
     """
 
-    L = np.zeros([2*sites, 2*sites], dtype=np.complex128)
+    # Create a list of the vectors describing the effect of the environment on each site in the Majorana representation
+    l_mu_list = []
 
-    for i in range(2*sites):
+    for i in range(2*sites - 2):
+        l_mu = np.zeros(2*sites, dtype=np.complex128)
         if i % 2 == 0:
-            L[i, i] = np.sqrt(gamma_g)/2
-            L[i + 1, i] = 1j*np.sqrt(gamma_g)/2
+            l_mu[i] = 1
+            l_mu[i + 1] = 1j
+            l_mu[i + 2] = 1j
+            l_mu[i + 3] = -1
+            l_mu *= np.sqrt(gamma_g)/2
         else:
-            L[i - 1, i] = np.sqrt(gamma_l)/2
-            L[i, i] = -1j*np.sqrt(gamma_l)/2
+            l_mu[i - 1] = 1
+            l_mu[i] = -1j
+            l_mu[i + 1] = -1j
+            l_mu[i + 2] = -1
+            l_mu *= np.sqrt(gamma_l)/2
+        l_mu_list.append(l_mu)
 
-    plt.matshow(L.real)
-    plt.colorbar()
-    plt.title('Real part of L')
-    plt.matshow(L.imag)
-    plt.colorbar()
-    plt.title('Imaginary part of L')
-    plt.show()
-    plt.close()
+    # Define the last two bath vectors
+    l_mu = np.zeros(2*sites, dtype=np.complex128)
+    l_mu[2*sites - 2] = 1
+    l_mu[2*sites - 1] = 1j
+    l_mu[0] = 1j
+    l_mu[1] = -1
+    l_mu *= np.sqrt(gamma_g)/2
+    l_mu_list.append(l_mu)
 
-    return L
+    l_mu = np.zeros(2*sites, dtype=np.complex128)
+    l_mu[2*sites - 2] = 1
+    l_mu[2*sites - 1] = -1j
+    l_mu[0] = -1j
+    l_mu[1] = -1
+    l_mu *= np.sqrt(gamma_l)/2
+    l_mu_list.append(l_mu)
+
+    # Initialize and calculate the matrix M as the sum over all the l_mu's of the Kronecker product of l_mu and l_mu^*
+    M = np.zeros([2*sites, 2*sites], dtype=np.complex128)
+    for k in range(2*sites):
+        M += np.kron(l_mu_list[k], l_mu_list[k].conj()).reshape(2*sites, 2*sites)
+
+    assert herm(M), 'The bath matrix is not Hermitian!'
+
+    visualization = False
+    if visualization:
+        fig, ax = plt.subplots()
+        ax.matshow(M.real)
+        for (i, j), z in np.ndenumerate(M.real):
+            ax.text(j, i, '{:0.3f}'.format(z), ha='center', va='center')
+        fig.suptitle('Real part of the dissipation matrix M, with $\gamma_g=%.2f$, $\gamma_l=%.2f$' % (gamma_g, gamma_l))
+        fig, ax = plt.subplots()
+        ax.matshow(M.imag)
+        for (i, j), z in np.ndenumerate(M.imag):
+            ax.text(j, i, '{:0.3f}'.format(z), ha='center', va='center')
+        fig.suptitle('Imaginary part of the dissipation matrix M, with $\gamma_g=%.2f$, $\gamma_l=%.2f$' % (gamma_g, gamma_l))
+        plt.show()
+        plt.close()
+
+    return M
 
 
-def dissipator(h: np.ndarray, L: np.ndarray, sites: int) -> (np.ndarray, np.ndarray):
+def correlation_matrix(H: np.ndarray, M: np.ndarray, sites: int) -> np.ndarray:
     """
-    Calculates the matrix z, solution of the continuous time Lyapunov equation, and verifies that
-    all the rapidities lie away from the imaginary axis.
+    Calculates the matrix Z, solution of the continuous time Lyapunov equation, and verifies that
+    all the rapidities lie away from the imaginary axis; then uses it to calculate the correlation matrix C.
 
     Parameters
     ----------
-    h: Kitaev Hamiltonian
-    l: bath operators
+    H: Kitaev Hamiltonian
+    M: dissipation matrix
+    sites: number of sites in the system
 
     Returns
     -------
-    The square matrix z, whose eigenvalues are the rapidities for the system.
+    The square antisymmetric correlation matrix C, containing all the information about the system in the NESS.
     """
 
-    m = np.zeros([2*sites, 2*sites], dtype=np.complex128)
+    X = -2.*1j*H + 2.*M.real
 
-    for i in range(2*sites):
-        for j in range(2*sites):
-            for k in range(2*sites):
-                m[i, j] += L[i, k]*L[j, k].conj()
+    Z = lyap(X.T, -M.imag)
+    assert (Z.T == -Z).all, "Matrix Z is not antisymmetric!"
 
-    assert herm(m), 'The bath matrix is not Hermitian!'
+    evals = la.eigvals(X)
+    assert all(evals), "The rapidities do not lie all away from the imaginary axis!"
 
-    plt.matshow(m.real)
-    plt.colorbar()
-    plt.title('Real part of M')
-    plt.matshow(m.imag)
-    plt.colorbar()
-    plt.title('Imaginary part of M')
-    plt.show()
-    plt.close()
-
-    x = -2*1j*h + 2*m.real
-
-    z = lyap(x, m.imag)
-    assert (z.transpose == -z).all, "Matrix Z is not antisymmetric!"
-
-    evals = la.eigvals(x)
-    assert all(evals), 'The rapidities do not lie all away from the imaginary axis!'
-
-    return z, m
+    C = np.identity(2*sites, dtype=np.complex128) + 4.*1j*Z
+    assert (C.T == -C).all, "The correlation matrix C is not antisymmetric!"
 
 
-def correlation_matrix(z: np.ndarray, sites: int) -> np.ndarray:
+    visualization = False
 
-    c = np.identity(2*sites, dtype=np.complex128) + 4*1j*z
-    assert (c.transpose == -z).all, "Correlation matrix is not antisymmetric!"
+    if visualization:
+        plt.matshow(C.real)
+        plt.colorbar()
+        plt.title('Real part of the correlation matrix')
+        plt.matshow(C.imag, cmap='inferno')
+        plt.colorbar()
+        plt.show()
+        plt.close()
 
-    return c
+    return C

kitaev/computers.py
@@ -1,28 +1,30 @@
 import numpy as np
+import numpy.linalg as la
 from pfapack import pfaffian as pf
-from assemblers import kit_hamiltonian, bath_operators, dissipator, correlation_matrix
+from assemblers import kit_hamiltonian, dissipator, liouvillean, correlation_matrix
 
 
 def compute_particle_density(mu: float, t: float, delta: float, gamma_g: float, gamma_l: float, sites: int) -> np.ndarray:
-    h = kit_hamiltonian(mu, t, delta, sites)
-    l = bath_operators(gamma_g, gamma_l, sites)
-    z, m = dissipator(h, l, sites)
+    H = kit_hamiltonian(mu, t, delta, sites)
+    M = dissipator(gamma_g, gamma_l, sites)
+    C = correlation_matrix(H, M, sites)
+
     density = np.zeros(sites, dtype=np.complex128)
     for i in range(sites):
-        density[i] = 1/2 - 1j*m[2*i, 2*i + 1]/2
+        density[i] = 0.5 - 0.5*1j*C[2*i, 2*i + 1]
 
     return density
 
 
-def compute_EGP(mu, t, delta, gamma, sites):
+def compute_EGP(mu, t, delta, gamma_g, gamma_l, sites):
 
     N = sites
 
-    # Build the correlation matrix M, with M_ij = i*(<w_i w_j>_NESS - delta_{ij}):
-    h = kit_hamiltonian(mu, t, delta, N)
-    l = bath_operators(gamma, N, sites)
-    z = dissipator(h, l, sites)
-    M = 1j*(correlation_matrix(z, N) - np.identity(2*sites, dtype=np.complex128))
+    # Build the covariance matrix M, with M_ij = i*(<w_i w_j>_NESS - delta_{ij}):
+    H = kit_hamiltonian(mu, t, delta, sites)
+    D = dissipator(gamma_g, gamma_l, sites)
+    C = correlation_matrix(H, D, sites)
+    M = 1j*(C - np.identity(2*sites, dtype=np.complex128))
     assert (M.transpose == -M).all, "Matrix M is not antisymmetric!"
 
     # Building matrices Ktilde and K2tilde:
@@ -66,7 +68,7 @@ def compute_EGP(mu, t, delta, gamma, sites):
             Omega = Omega * np.cos(np.pi * (k + 1) / N)
 
     # Check whether M is singular:
-    if np.linalg.matrix_rank(M) < 2 * N:
+    if np.linalg.matrix_rank(M) < 2*N:
         print('The covariance matrix M is singular! It cannot be inverted!')
         exit()
 

kitaev/visualization.py
@@ -1,25 +1,31 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-from assemblers import kit_hamiltonian, bath_operators, dissipator, correlation_matrix
+from assemblers import kit_hamiltonian, dissipator, correlation_matrix
 from computers import compute_EGP, compute_particle_density
+
+matplotlib.rcParams.update({'font.size': 8})
 
 
 def plot_density(mu: float, t: float, delta: float, gamma_g: float, gamma_l: float, sites: int):
     density = compute_particle_density(mu, t, delta, gamma_g, gamma_l, sites)
     plt.scatter(np.arange(sites), density)
     plt.title('Particle density for each site in a Kitaev chain with\n'
-              '$\mu=%.2f$, $t=%.2f$, $\Delta=%.2f$, $\gamma_g=%.2f$, $\gamma_l=%.2f$ and %d sites' % (mu, t, delta, gamma_g, gamma_l, sites))
-    #plt.ylim([-0.6,0.6])
+              '$\mu=%.2f$, $t=%.2f$, $\Delta=%.2f$, $\gamma_g=%.2f$, $\gamma_l=%.2f$ '
+              'and %d sites' % (mu, t, delta, gamma_g, gamma_l, sites))
+    plt.ylim([-0.1,1.2])
+    plt.xlabel('Sites')
+    plt.ylabel('Average particle density')
     plt.show()
     plt.close()
 
     return
 
 
-def plot_correlation(mu: float, t: float, delta: float, gamma1: float, gamma2: float, gamma_points: int, sites: int):
+def plot_correlation(mu: float, t: float, delta: float, gamma_g: float, gamma_l: float,
+                     delta_points: int, sites: int):
     """
-    Plots the real and imaginary parts of the correlation matrix for the Kitaev chain, for various
-    values of the coupling constant gamma, ranging from gamma1 to gamma2.
+    Plots the real and imaginary parts of the correlation matrix for the Kitaev chain..
     Parameters
     ----------
     mu: onsite potential
@@ -34,56 +40,50 @@ def plot_correlation(mu: float, t: float, delta: float, gamma1: float, gamma2: f
     -------
     Nothing, but plots the correlation matrix.
     """
-    gamma_array = np.linspace(gamma1, gamma2, gamma_points)
-
-    h = kit_hamiltonian(mu, t, delta, sites)
-
-    fig, axs = plt.subplots(gamma_points, 2, figsize=(12, 12))
-    fig.suptitle('Real and imaginary parts of the correlation matrix for the Kitaev chain\n'
-                 'with $\mu=%.2f$, $t =%.2f$, $\Delta=%.2f$ and various choices of $\gamma$ ranging\n'
-                 'from $\gamma_1=%.2f$ to $\gamma_2=%.2f$' % (mu, t, delta, gamma1, gamma2))
-    for i in range(gamma_points):
-        gamma = gamma_array[i]
-        l = bath_operators(gamma, sites)
-        z = dissipator(h, l)
-        c = correlation_matrix(z, sites)
-        c_real = c.real
-        c_imag = c.imag
-        ax1 = axs[i, 0].imshow(c_real)
-        axs[i, 0].set_title('Real part, $\gamma=%.2f$' % gamma)
-        fig.colorbar(ax1)
-        ax2 = axs[i, 1].imshow(c_imag)
-        axs[i, 1].set_title('Imaginary part, $\gamma=%.2f$' % gamma)
-        fig.colorbar(ax2)
+    H = kit_hamiltonian(mu, t, delta, sites)
+    M = dissipator(gamma_g, gamma_l, sites)
+    C = correlation_matrix(H, M, sites)
+    plt.matshow(C.real, cmap='inferno')
+    plt.colorbar()
+    plt.title('Real part of the correlation matrix\n'
+              'for a Kitaev chain with $\mu=%.2f$, $t=%.2f$, $\Delta=%.2f$,\n'
+              '$\gamma_g=%.2f$, $\gamma_l=%.2f$ and %d sites\n' % (mu, t, delta, gamma_g, gamma_l, sites))
+    plt.matshow(C.imag, cmap='inferno')
+    plt.colorbar()
+    plt.title('Imaginary part of the correlation matrix\n'
+              'for a Kitaev chain with $\mu=%.2f$, $t=%.2f$, $\Delta=%.2f$,\n'
+              '$\gamma_g=%.2f$, $\gamma_l=%.2f$ and %d sites\n' % (mu, t, delta, gamma_g, gamma_l, sites))
     plt.show()
     plt.close()
 
     return
 
 
-def plot_egp(mu: float, t: float, delta1: float, delta2: float, delta_points: int, gamma1: float, gamma2: float, gamma_points: int, sites: int):
+def plot_egp(mu: float, t: float, delta1: float, delta2: float, delta_points: int, gamma_g1: float, gamma_g2: float,
+             gamma_l: float, gamma_points: int, sites: int):
 
     delta_array = np.linspace(delta1, delta2, delta_points)
-    gamma_array = np.linspace(gamma1, gamma2, gamma_points)
+    gamma_array = np.linspace(gamma_g1, gamma_g2, gamma_points)
     egp_array = np.zeros([delta_points, gamma_points])
 
     for delta_idx, delta in enumerate(delta_array):
-        for gamma_idx, gamma in enumerate(gamma_array):
-            u_real, u_imag = compute_EGP(mu, t, delta, gamma, sites)
+        for gamma_idx, gamma_g in enumerate(gamma_array):
+            u_real, u_imag = compute_EGP(mu, t, delta, gamma_g, gamma_l, sites)
             egp_array[delta_idx, gamma_idx] = np.imag(np.log(u_real + 1j*u_imag))
             print((delta_idx*gamma_points+gamma_idx)/(delta_points*gamma_points)*100, '% of calculation complete')
 
-    x, y = np.meshgrid(delta_array/t, gamma_array/t)
+    x, y = np.meshgrid(delta_array/t, gamma_array/gamma_l)
     z = egp_array/np.pi
     fig = plt.figure(figsize=(9, 6))
     fig.suptitle('EGP phase diagram for a Kitaev chain with %d sites and $\mu=%.2f$,\n'
-                 '$t=%.2f$, $\Delta_1=%.2f$, $\Delta_2=%.2f$, $\gamma_1=%.2f$ and $\gamma_2=%.2f$' % (sites, mu, t, delta1, delta2, gamma1, gamma2))
+                 '$t=%.2f$, $\Delta_1=%.2f$, $\Delta_2=%.2f$, $\gamma_{g1}=%.2f$, $\gamma_{g2}=%.2f$ and '
+                 '$\gamma_l = %.2f$' % (sites, mu, t, delta1, delta2, gamma_g1, gamma_g2, gamma_l))
     ax1 = fig.gca()
     contour = ax1.pcolormesh(x, y, z, cmap='viridis')
     ax1.grid()
     ax1.set_xlabel(r'$\Delta/t$')
-    ax1.set_ylabel(r'$\gamma/t$')
-    cbar = [fig.colorbar(contour, shrink=0.5, aspect=5)]
+    ax1.set_ylabel(r'$\gamma_g/\gamma_l$')
+    cbar = [fig.colorbar(contour)]
     plt.show()
     plt.close()
 